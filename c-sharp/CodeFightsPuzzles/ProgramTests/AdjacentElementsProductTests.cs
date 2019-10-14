bc476882ffdd2d9e9f211d1f56a7976a432eed19
mhamner <37221769+mhamner@users.noreply.github.com>
2019-10-14 17:25:29 -0400

    Add files via upload

---
+++ c-sharp/CodeFightsPuzzles/ProgramTests/AdjacentElementsProductTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFightsPuzzles;
+
+namespace ProgramTests
+{
+    [TestClass]
+    public class AdjacentAreaTests
+    {
+        [TestMethod]
+        public void Pass_Letters_in_Array()
+        {
+            //Verify that we fail if we try and pass letters in the array
+            string n = "A,B,C,D,E";
+            string[] elements = n.Split(',');
+                    
+            //Test passes if we fail for Format Exception as expected
+            Assert.ThrowsException<System.FormatException>(()
+                => AdjacentElementsProduct.adjacentElementsProduct(elements));
+            
+
+        }
+    }
+}
---
+++ c-sharp/CodeFightsPuzzles/ProgramTests/CheckPalindromeTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeFightsPuzzles;
+
+namespace ProgramTests
+{
+    [TestClass]
+    public class CheckPalindromeTests
+    {
+        
+        [TestMethod]
+        public void Test_Isnt_a_Palindrome()
+        {
+            string s = "notapalindrome";
+            //Assert that this should be false; test will fail if it comes back true
+            Assert.IsFalse(CheckPalindrome.checkPalindrome(s));
+        }
+
+        [TestMethod]
+        public void Test_Is_a_Palindrome()
+        {
+            string s = "bob";
+            //Assert that this should be true; test will fail if it comes back false
+            Assert.IsTrue(CheckPalindrome.checkPalindrome(s));
+        }
+
+    }
+}
